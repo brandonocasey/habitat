b0ce1b5afdb81b073a5e891bbaf0091eb5f05354
Brandon Casey <brandonocasey@gmail.com>
2014-09-05 16:34:44 -0400

    Updated runner to use things that will work for this project

--- runner.sh
+++ runner.sh
@@ -3,27 +3,16 @@
 # implement getopts
 # subset, search, etc
 
-download() {
-    name="$1"; shift;
-    url="$1"; shift;
-    if command -v wget > /dev/null; then
-        wget -O "$name" "$url" -q --no-check-certificate
-    elif command -v curl > /dev/null; then
-        curl -o "$name" "$url" -q
-    else
-        echo "We require curl or wget to download scripts"
-        exit
-    fi
-}
 
-
-t_dir="$( cd "$( dirname "$0" )" && pwd )"
-dot_dir="$( cd "$( dirname "$t_dir" )" && pwd )"
-test_dir="$t_dir/tests"
-tmp_dir="$t_dir"/tmp
+script_dir="$( cd "$( dirname "$0" )" && pwd )"
+test_dir="$script_dir/t"
+tmp_dir="$script_dir/storage/tmp"
 
 assert_script="$t_dir/assert.sh"
 stub_script="$t_dir/stub.sh"
+
+
+source "$script_dir/download"
 if [ ! -f "$assert_script" ]; then
     download "$assert_script" "https://raw.githubusercontent.com/lehmannro/assert.sh/master/assert.sh"
 fi
@@ -60,38 +49,7 @@ if [ "${#subset[@]}" -eq "0" ]; then
 fi
 
 for FILE in "${subset[@]}"; do
-    # Cleanup
-    if [ ! -d "$tmp_dir" ]; then
-        mkdir -p "$tmp_dir"
-    fi
-    rm -rf "$tmp_dir"/*
-
-    # What file does this file test?
-
-    # get the path without test directory appended or the final file
-    file_path="$(echo "$( dirname "$FILE")" | sed -e "s~$test_dir/~~")"
-    # add the final file back without an extensipn
-    file_path+="/$(basename "${FILE%.*}")"
-
-    source_file=""
-
-    # search for the file the user wants to source
-    search="$dot_dir/$file_path"
-    while [ -z "$source_file" ] && [ "$search" != "$dot_dir" ]; do
-        # exapnd .* if possible
-        search_file="$( echo "$search".* )"
-
-        # if expanded whe know what they want to source
-        if [ "$search_file" != "$search.*" ]; then
-            source_file="$search_file"
-        fi
-        search="$(dirname "$search")"
-    done
-    if [ -n "$source_file" ]; then
-        echo "*** Running Test $file_path ***"
-        (source "$source_file"; source "$FILE"; assert_end "$file_path";)
-        echo
-    else
-        echo "$file_path skipped, we could not find a file to source for it"
-    fi
+    echo "*** Running Test $file_path ***"
+    (source "$FILE"; assert_end "$file_path";)
+    echo
 done
