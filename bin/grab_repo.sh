510777ea39d363f8855db0300d8eb827c4555166
Brandon Casey <brandonocasey@gmail.com>
2014-09-10 08:54:30 -0400

    grab repo is now working with github links

--- bin/grab_repo.sh
+++ bin/grab_repo.sh
@@ -6,6 +6,12 @@ if [ -z "$1" ] || [ -z "$2" ]; then
 fi
 url_or_git_repo="$1"; shift
 output_dir="$1"; shift
-if [ -n "$(echo "$url_or_git_repo" | grep -E '^(git|http)://')" ]; then
-	cd $output_dir && git pull >> "$custom_log_file"
+url=""
+if [ -n "$(echo "$url_or_git_repo" | grep -E '^(git|http|https)://')" ]; then
+	url="$url_or_git_repo"
+elif [ -n "$(echo "$url_or_git_repo" | grep -E '^.*/.*$')" ]; then
+	url="$(echo "$url_or_git_repo" | sed -e 's~ ~~g')"
+	url="https://github.com/${url}.git"
 fi
+
+(cd "$output_dir" && git clone "$url")
