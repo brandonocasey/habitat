37823283963938b41e55211907cadf9bd5c95c14
Brandon Casey <brandonocasey@gmail.com>
2014-09-10 11:10:17 -0400

    update repo now returns an error if no repo was found

--- bin/update_repo.sh
+++ bin/update_repo.sh
@@ -12,12 +12,18 @@ if [ -z "$1" ]; then
 	exit 2
 fi
 
+code="1"
 dir="$1"; shift
 if [ -d "$dir/.svn" ]; then
 	svn up $dir
+	code="0"
 fi
 if [ -d "$dir/.git" ]; then
 	(cd $dir && git pull)
+	code="0"
 fi
 
-
+if [ "$code" -eq "1" ]; then
+	echo "Repo in $dir was not found"
+fi
+exit "$code"
