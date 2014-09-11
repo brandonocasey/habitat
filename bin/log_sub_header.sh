e6a3294578e5c07ff15d83ad2647b359682a9775
Brandon Casey <brandonocasey@gmail.com>
2014-09-11 15:07:27 -0400

    Added a binary for logging extension steps/names

---
+++ bin/log_sub_header.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+if [ "$#" -ne "1" ] || [ -z "$1" ]; then
+    echo "Must pass a non-blank variable to log_header"
+    exit 2
+fi
+if [ -n "$custom_log_file" ]; then
+    if [ ! -f "$custom_log_file" ]; then
+        : > "$custom_log_file"
+    fi
+    printf "\n$1\n" >> "$custom_log_file"
+else
+    printf "\n$1\n"
+fi
+
+exit 0
