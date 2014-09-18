9e88e4ecca2f00afb8dae1c47508b9e6b8a670a5
Brandon Casey <brandonocasey@gmail.com>
2014-09-18 17:02:29 -0400

    Started the array script

--- new_bin/array.sh
+++ new_bin/array.sh
@@ -12,33 +12,60 @@
 
 function push() {
 
+    echo "NYI"
 }
 
 function shift() {
 
+    echo "NYI"
 }
 
 function pop() {
+    echo "NYI"
 
 }
 
 function unshift() {
+    echo "NYI"
 
 }
 
 function splice() {
+    echo "NYI"
 
 }
 
 function index() {
-
+    echo "NYI"
 }
 
 function all() {
+    local array="$1"; shift
+    (
+        IFS="|"
+        for value in $array; do
+            echo "$value"
+        done
+    )
+}
 
+
+
+function length() {
+    echo "NYI"
 }
 
 # return index or undef
-function find() {
-
+function exists() {
+    local array="$1"; shift
+    local value="$1"; shift
+    if [ -n "$(echo "$array" | grep -E "(^|\|)$value($|\|)")" ]; then
+        return 0
+    fi
+    return 1
 }
+
+array="asd|dsa|ddd"
+
+all "$array"
+exists "$array" "dsa"
