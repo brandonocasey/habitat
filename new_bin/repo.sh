8cbbad01307dbfac8ec43b74e892c9d9110d684c
Brandon Casey <brandonocasey@gmail.com>
2014-09-22 09:33:46 -0400

    Finished the repo binary

--- new_bin/repo.sh
+++ new_bin/repo.sh
@@ -14,51 +14,67 @@ SVN_TYPE=2
 MER_TYPE=3
 
 
-function check_repo_status() {
+function status() {
     local repo_dir="$1"; shift
-    local repo_type="$1"; shift
+    local local_rev
+    local remote_rev
+    local base_rev
 
     # http://stackoverflow.com/a/3278427
     if [ "$repo_type" -eq "$GIT_TYPE" ]; then
-        local local_rev="$(cd "$repo_dir" && git rev-parse @{0})"
-        local remote_rev="$(cd "$repo_dir" && git rev-parse @{u})"
-        local base_rev="$(cd "$repo_dir" && git merge-base @{0} @{u})"
+        local_rev="$(cd "$repo_dir" && git rev-parse @{0})"
+        remote_rev="$(cd "$repo_dir" && git rev-parse @{u})"
+        base_rev="$(cd "$repo_dir" && git merge-base @{0} @{u})"
 
-        if [ "$local_rev" = "$remote_rev" ]; then
-            echo "Up-to-date"
-        elif [ "$local_rev" = "$base_rev" ]; then
-            echo "Need to pull"
-        elif [ "$remote_rev" = "$base_rev" ]; then
-            echo "Need to push"
-        else
-            echo "Diverged"
+        if [ "$local_rev" = "$base_rev" ]; then
+            # Need to pull
+            exit 0
+        fi
+    elif [ "$repo_type" -eq "$SVN_TYPE" ]; then
+        local_rev="$(svn info | grep -i "Last Changed Rev")"
+        remote_rev="$(svn info -r HEAD | grep -i "Last Changed Rev")"
+
+        if [ "$local_rev" != "$remote_rev" ]; then
+            exit 0
         fi
     fi
+
+    # up to date or not a repo
+    exit 1
 }
 
-function get_repo_type() {
-    local repo_dir="$1"; shift
-    if [ -d "$repo_dir/.git" ]; then
+function type() {
+    local repo="$1"; shift
+    if [ -n "$(echo "$repo" | grep -E "^(https|git)://.*.git$")" ]; then
         echo "$GIT_TYPE"
-    elif [ -d "$repo_dir/.svn" ]; then
+    elif [ -n "$(echo "$repo" | grep -E "^svn+ssh")" ]; then
         echo "$SVN_TYPE"
+    elif [ -d "$repo" ]; then
+        if [ -d "$repo_dir/.git" ]; then
+            echo "$GIT_TYPE"
+        elif [ -d "$repo_dir/.svn" ]; then
+            echo "$SVN_TYPE"
+        fi
+    elif [ -n "$(echo "$repo" | grep ".*/.*")" ]; then
+        echo "$GIT_TYPE"
+    else
+        validation_error "Unable to determine repo type of $repo"
     fi
 }
 
-function update_repo() {
+function update() {
     local repo_dir="$1"; shift
-    local repo_type="$1"; shift
 
     if [ "$repo_type" -eq "$GIT_TYPE" ]; then
         (cd "$repo_dir" && git pull "$repo_dir")
     elif [ "$repo_type" -eq "$SVN_TYPE" ]; then
         svn up "$repo_dir"
     fi
+    exit 0
 }
 
-function download_repo() {
+function download() {
     local repo_url="$1"; shift
-    local repo_type="$1"; shift
 
     if [ "$repo_type" -eq "$GIT_TYPE" ]; then
         (cd "$repo_dir" && git pull "$repo_dir")
@@ -70,9 +86,11 @@ function download_repo() {
 
 action=""
 help=""
-help+="match <string> <regex>, 0 if matches 1 otherwise$nl"
-help+="replace <string> <regex_search> <replacement> replace from a string$nl"
-help+="i insensitive case matching$nl"
+help+="download <url> download a repo$nl"
+help+="status   <dir> get the status of a rrepo$nl"
+help+="# return code of 1 indicates everything is up to date$nl"
+help+="# return code of 0 indicates updates are possible$nl"
+help+="update   <dir> update a repo$nl"
 while [ "$#" -gt "0" ]; do
     arg="$1"; shift
     case $arg in
@@ -82,26 +100,28 @@ while [ "$#" -gt "0" ]; do
         --)
             insensitive="0"
         ;;
-        --match)
+        --download|--status|--update)
         if [ -n "$action" ]; then
             argument_error "Cannot do $arg and $action"
         fi
-        if [ -z "$1" ] || [ -z "$2" ]; then
-            argument_error "$arg requires two arguments"
-        fi
-        action="$arg"
-        ;;
-        --replace)
         if [ -z "$1" ]; then
-            argument_error "Must have an argument after $arg"
-        fi
-        if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-            argument_error "$arg requires three arguments"
+            argument_error "$arg requires an argument"
         fi
         action="$arg"
+        arg1="$1"; shift
         ;;
         *)
             argument_error "Invalid Argument $arg"
         ;;
     esac
 done
+
+repo_type=$(type "$arg1")
+
+if [ "$action" = "--download" ]; then
+    download "$arg1"
+elif [ "$action" = "--status" ]; then
+    status "$arg1"
+elif [ "$action" = "--update" ]; then
+    update "$arg1"
+fi
