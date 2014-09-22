620285dc6bbe2b86bf9a0036b1e8942c73a653e2
Brandon Casey <brandonocasey@gmail.com>
2014-09-22 13:50:26 -0400

    New binaries are ready for unit tests

--- bin/isset.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-eval "local thing=\${$1:-}"
-if [ ! -z "${thing:-}" ]; then
-    exit 0
-fi
-exit 1
--- bin/log.sh
+++ bin/log.sh
@@ -1,23 +1,172 @@
 #! /bin/sh
+source "$( cd "$(dirname $0 )/.." && pwd)/lib/main.sh"
 
-if [ -z "${1:-}" ]; then
-    while read var; do
-        log "$var"
-    done
-    return
-fi
-for var in "$@"; do
-    var="$(echo "$var" | sed -e "s#$HOME#~#g")"
-    var="[$(date)] $var"
-    # newline is seprate so that trailing newlines don't escape it
-    if [ -n "$custom_log_file" ]; then
-        if [ ! -f "$custom_log_file" ]; then
-            :  > "$custom_log_file"
-        fi
-        printf "$var" >> "$custom_log_file"
-        printf "\n" >> "$custom_log_file"
+function get_prefix() {
+    local prefix=""
+    if [ "$action" = "--stdout" ]; then
+        prefix=""
+    elif [ -z "$CUSTOM_LOG_PREFIX" ]; then
+        prefix="$(eval "$CUSTOM_LOG_PREFIX")"
     else
-        printf "$var"
-        printf "\n"
+        prefix="$(date): "
     fi
+    echo "$prefix"
+}
+
+# 0 no prefix
+# 1 header
+# 2 prefix
+function log() {
+    local setting="$1"; shift
+    local line=""
+    (while read -r line;do
+        if [ -n "$line" ]; then
+            if [ "$setting" -eq "1" ]; then
+                line="*----$line----*"
+            elif [ "$setting" -eq "2" ]; then
+                line="$(get_prefix) $line"
+            fi
+            echo "$line"
+        fi
+    done <<< "$log_lines")
+} 1>&3
+
+
+# Run the command and pipe the output to this binary
+# have this binary print to stdout
+# redirect stdout to the log
+function log_result() {
+    "$@" 2>&1 | "$0 --stdout"
+} 1>&3
+
+function log_header() {
+    (while read -r line;do
+        if [ -n "$line" ]; then
+            echo "*----$line----*" 2>&1
+        fi
+    done <<< "$log_lines")
+} 1>&3
+
+
+log_lines=""
+# if stdin is a tty: process command line
+if [ ! -t 0 ]; then
+    set -- "$@" "$var"
+fi
+
+action=""
+file=""
+
+help=""
+help+="# Anything passed without -- is assumed to be a log line/part of a command$nl"
+help+="# Environment Variables:$nl"
+help+="# CUSTOM_LOG_FILE - file to log to or stdout for stdout$nl"
+help+="# CUSTOM_LOG_LEVEL - Current log level default is 2$nl"
+help+="# CUSTOM_LOG_PREFIX - Custom log prefix$nl"
+
+help+="file The file to log export CUSTOM_LOG_FILE$nl"
+
+
+help+="header Print log lines in header format$nl"
+help+="stdout Log to stdout instead of file$nl"
+help+="result Log the result of a command$nl"
+help+="error log to level 1$nl"
+help+="info  log to level 2$nl"
+help+="debug log to level 3$nl"
+help+="devel log to level 4$nl"
+while [ "$#" -gt "0" ]; do
+    arg="$1"; shift
+    case $arg in
+        --help)
+            usage "$help"
+        ;;
+        --error)
+            if [ -n "$level" ]; then
+                argument_error "Cannot print to $level and $arg"
+            fi
+            level="1"
+        ;;
+       --info)
+            if [ -n "$level" ]; then
+                argument_error "Cannot print to $level and $arg"
+            fi
+            level="2"
+        ;;
+        --debug)
+            if [ -n "$level" ]; then
+                argument_error "Cannot print to $level and $arg"
+            fi
+            level="3"
+        ;;
+         --devel)
+            if [ -n "$level" ]; then
+                argument_error "Cannot print to $level and $arg"
+            fi
+            level="4"
+        ;;
+        --header|--stdout|--result)
+            if [ -n "$action" ]; then
+                argument_error "Cannot run $action and $arg"
+            fi
+            action="$arg"
+        --file)
+            if [ -z "$1" ]; then
+                argument_error "Must have an argument after $arg"
+            fi
+            file="$1"; shift
+        ;;
+        *)
+
+            if [ -z "$log_lines" ]; then
+                log_lines+="${arg}"
+            else
+                log_lines+="${nl}${arg}"
+            fi
+        ;;
+    esac
 done
+
+if [ -z "$level" ]; then
+    level="2"
+fi
+if [ -z "$CURRENT_LOG_LEVEL" ]; then
+    current_level="2"
+else
+    current_level="$CURRENT_LOG_LEVEL"
+fi
+
+if [ -z "$log_lines" ]; then
+    validation_error "You must pass in lines to log or a command"
+fi
+
+if [ "$action" != "--stdout" ]; then
+    if [ -z "$file" ] && [ -z "$CUSTOM_LOG_FILE" ]; then
+        validation_error "A Log file must be set with --file or CUSTOM_LOG_FILE"
+    elif [ -z "$file" ]; then
+        log_file="$CUSTOM_LOG_FILE"
+    fi
+    exec 3>> "$file"
+fi
+
+log_settings="0"
+if [ "$action" = "--result" ]; then
+    log_result "$log_lines"
+elif [ "$action" = "--stdout" ]; then
+    exec 3>&1
+    log.sh "2"
+else
+    if [ "$current_level" -ge "$level" ]; then
+        if [ "$action" = "--header" ]; then
+            log_settings="2"
+        fi
+        log "$log_settings"
+        if [ "$level" = "--error" ]; then
+            exec 3>&2
+            log "$log_settings"
+        fi
+    fi
+fi
+
+
+exec 3>&-
+exit 0
