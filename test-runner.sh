167dddae1f2543f5f426313a1d54e9251c708bae
Brandon Casey <brandonocasey@gmail.com>
2015-03-06 22:43:58 -0500

    re-wrote to correctly run tests

--- test-runner.sh
+++ test-runner.sh
@@ -1,9 +1,6 @@
-if [ ! -f "$assert_script" ] || [ ! -f "$stub_script" ]; then
-    wget "https://raw.githubusercontent.com/lehmannro/assert.sh/master/assert.sh" "$assert_script"
-    wget  "https://raw.githubusercontent.com/jimeh/stub.sh/master/stub.sh" "$stub_script"
-fi
+#!/usr/bin/env bash
 
-for unit_test_file in $(find "$unit_test_dir" -name "*.t.sh"); do
-    unit_test="$(basename "$unit_test_file")"
-    echo "*** Running Test $unit_test ***"
+for unit_test_file in $(find t -name "*.t.sh"); do
+    echo "*** Running Test $(basename "$unit_test_file") ***"
+    $unit_test_file
 done
