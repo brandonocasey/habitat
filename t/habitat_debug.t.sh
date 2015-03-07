fe7d3a4b7dc2c1174b9ade5655fe235c909c2834
Brandon Casey <brandonocasey@gmail.com>
2015-03-07 01:43:13 -0500

    added test name

--- t/habitat_debug.t.sh
+++ t/habitat_debug.t.sh
@@ -1,19 +1,33 @@
 #!/usr/bin/env bash
-source "$(dirname "$0")/test-helper.sh" "$0"
-restore habitat_debug
-function="habitat_debug"
-restore "$function"
+source "$(dirname "$0")/test-helper.sh" "$0" "$@"
+func="habitat_debug"
+restore "$func"
 
-# with debug off
-assert_raises "$function '' '' ''" "0"
-assert "$function '' ''"
-assert "$function 'hello'"
-assert_raises "$function 'hello'" "0"
+
+test_name "Blank input does nothing with debug off"
+assert_raises "$func '' '' ''" "0"
+
+test_name "Blank input prints nothing with debug off"
+assert "$func '' ''"
+
+test_name "input with debug off prints nothing"
+assert "$func 'hello'"
+
+test_name "input with debug off is successful"
+assert_raises "$func 'hello'" "0"
 
 # with debug on
 habitat_debug_output=1
-assert "$function 'hello'" "Debug: hello"
-assert_raises "$function 'hello'" "0"
-assert "$function '' ''"
+test_name "input with debug on prints"
+assert "$func 'hello'" "Debug: hello"
 
-assert_end "$function"
+test_name "input with debug is successful"
+assert_raises "$func 'hello'" "0"
+
+test_name "input with multi line input and debug is prints"
+assert "$func 'hello' 'hello'" "Debug: hello\nDebug: hello"
+
+test_name "Blank input with debug does nothing"
+assert "$func '' ''"
+
+assert_end "$func"
