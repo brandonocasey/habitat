5c292c61041b238487852d39b1c17f22a0b1b446
Brandon Casey <brandonocasey@gmail.com>
2015-03-06 22:06:21 -0500

    added unit tests for habitat_cleanup

--- t/habitat_cleanup.t.sh
+++ t/habitat_cleanup.t.sh
@@ -1,20 +1,87 @@
 #!/usr/bin/env bash
 source "test-helper.sh" "$0"
-function="habitat_cleanup"
-stub 'habitat_unset_function'
-stub 'habitat_unset_variable'
+func="habitat_cleanup"
 
-$function
+
+function setup() {
+  stub 'habitat_unset_function'
+  stub 'habitat_unset_variable'
+}
+function clean() {
+  restore 'habitat_unset_function'
+  restore 'habitat_unset_variable'
+}
+
+
+#
+#
+# Make sure we cleanup habitat_
+#
+#
+setup
+$func
 # check if the number doubled
 assert_raises "stub_called 'habitat_unset_function'" "0"
 assert_raises "stub_called 'habitat_unset_variable'" "0"
 
-# now we have the base, when
-unset_function_calls="$(stub_called_times "habitat_unset_function")"
-unset_variable_calls="$(stub_called_times "habitat_unset_variable")"
+# now we have the functions/variables that will be unset every time
+function_start="$(stub_called_times "habitat_unset_function")"
+variable_start="$(stub_called_times "habitat_unset_variable")"
+clean
 
 
 
 
 
-assert_end "$function"
+#
+#
+# Make sure we cleanup the same amount twice
+#
+#
+setup
+$func
+assert "stub_called_times 'habitat_unset_function'" "$function_start"
+assert "stub_called_times 'habitat_unset_variable'" "$variable_start"
+clean
+
+
+
+
+#
+#
+# Make sure we cleanup an extra function/variable
+#
+#
+setup
+function habitat_unset_me() {
+  :
+}
+habitat_var_unset_me=""
+$func
+assert "stub_called_times 'habitat_unset_function'" "$((function_start+1))"
+assert "stub_called_times 'habitat_unset_variable'" "$((variable_start+1))"
+clean
+
+
+#
+#
+# Make sure we cleanup an up two extra function/variable
+#
+#
+setup
+function habitat_unset_me() {
+  :
+}
+function habitat_unset_me_two() {
+  :
+}
+habitat_var_unset_me=""
+habitat_var_unset_me_two=""
+$func
+assert "stub_called_times 'habitat_unset_function'" "$((function_start+2))"
+assert "stub_called_times 'habitat_unset_variable'" "$((variable_start+2))"
+clean
+
+
+
+assert_end "$func"
