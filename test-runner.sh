3bcdea8508f732e542b1140ebf06673f77b5a685
Brandon Casey <brandonocasey@gmail.com>
2015-03-07 01:36:53 -0500

    added the ability to pass args to tests, and run from anywhere

--- test-runner.sh
+++ test-runner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for unit_test_file in $(find t -name "*.t.sh"); do
+for unit_test_file in $(find "$(dirname "$0")"/t -name "*.t.sh"); do
     echo "*** Running Test $(basename "$unit_test_file") ***"
-    $unit_test_file
+    $unit_test_file "$@"
 done
