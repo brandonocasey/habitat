a30efc08ad3e3aa717bacd2783e2dfa4e10a976b
Brandon Casey <brandonocasey@gmail.com>
2015-03-06 23:33:58 -0500

    gave the variables and function unique names

--- t/habitat_cleanup.t.sh
+++ t/habitat_cleanup.t.sh
@@ -6,6 +6,8 @@ func="habitat_cleanup"
 function setup() {
   stub 'habitat_unset_function'
   stub 'habitat_unset_variable'
+  $func
+  assert "echo '$?'" "0"
 }
 function clean() {
   restore 'habitat_unset_function'
@@ -19,7 +21,6 @@ function clean() {
 #
 #
 setup
-$func
 # check if the number doubled
 assert_raises "stub_called 'habitat_unset_function'" "0"
 assert_raises "stub_called 'habitat_unset_variable'" "0"
@@ -39,7 +40,6 @@ clean
 #
 #
 setup
-$func
 assert "stub_called_times 'habitat_unset_function'" "$function_start"
 assert "stub_called_times 'habitat_unset_variable'" "$variable_start"
 clean
@@ -52,12 +52,11 @@ clean
 # Make sure we cleanup an extra function/variable
 #
 #
-setup
-function habitat_unset_me() {
+habitat_unset_me_one() {
   :
 }
-habitat_var_unset_me=""
-$func
+habitat_var_unset_me_one=""
+setup
 assert "stub_called_times 'habitat_unset_function'" "$((function_start+1))"
 assert "stub_called_times 'habitat_unset_variable'" "$((variable_start+1))"
 clean
@@ -68,18 +67,17 @@ clean
 # Make sure we cleanup an up two extra function/variable
 #
 #
-setup
-function habitat_unset_me() {
+habitat_unset_me_two() {
   :
 }
-function habitat_unset_me_two() {
+habitat_unset_me_three() {
   :
 }
-habitat_var_unset_me=""
 habitat_var_unset_me_two=""
-$func
-assert "stub_called_times 'habitat_unset_function'" "$((function_start+2))"
-assert "stub_called_times 'habitat_unset_variable'" "$((variable_start+2))"
+habitat_var_unset_me_three=""
+setup
+assert "stub_called_times 'habitat_unset_function'" "$((function_start+3))"
+assert "stub_called_times 'habitat_unset_variable'" "$((variable_start+3))"
 clean
 
 
