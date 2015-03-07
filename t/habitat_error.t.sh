e6a03912ef4744b47e07bacf17ccbb09250d32b1
Brandon Casey <brandonocasey@gmail.com>
2015-03-07 01:57:57 -0500

    added more tests and added test_name

--- t/habitat_error.t.sh
+++ t/habitat_error.t.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
+source "$(dirname "$0")/test-helper.sh" "$0" "$@"
+func="habitat_error"
+restore "$func"
 
-source "$(dirname "$0")/test-helper.sh" "$0"
-restore "habitat_error"
-function="habitat_error"
-restore "$function"
+test_name "Blank input is successful"
+assert_raises "$func '' '' ''" "0"
 
-# with blank args
-assert_raises "$function '' '' ''" "0"
-assert "$function '' '' '' 2>&1"
-assert "$function 'hello'"
-assert "$function 'hello' 2>&1" "Error: hello"
-assert_raises "$function 'hello'" "0"
-assert_end "$function"
+test_name "Blank input prints nothing"
+assert "$func '' ''" ""
+
+test_name "input prints to stderr"
+assert "$func 'hello' 2>&1" 'Error: hello'
+
+test_name "input doesnt print to stdout"
+assert "$func 'hello'" ''
+
+test_name "input is successful"
+assert_raises "$func 'hello'" "0"
+
+test_name "multiline input prints to stderr"
+assert "$func 'hello' 'hello2' 2>&1" "Error: hello\nError: hello2"
+
+test_name "multiline input doesnt print to stdout"
+assert "$func 'hello' 'hello2'" ''
+
+test_name "multiline input is successful"
+assert_raises "$func 'hello' 'hello2'" "0"
+
+assert_end "$func"
