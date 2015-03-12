484e32848244d62785f2c56b3c4e1d18afc4cf20
Brandon Casey <brandonocasey@gmail.com>
2015-03-11 23:52:37 -0400

    Fully tested and fixed the stub function

--- t/habitat_stub.t.sh
+++ t/habitat_stub.t.sh
@@ -7,12 +7,13 @@ func="habitat_stub"
 
 
 function setup() {
-  :
+  stub "habitat_is_valid_plugin_name"
 }
 function clean() {
   if [ -d "$tmp" ]; then
     rm -rf "$tmp"/*
   fi
+  restore "habitat_is_valid_plugin_name"
 }
 
 
@@ -23,7 +24,9 @@ setup
 plugin=""
 installation_dir=""
 test_name "no plugin and no installation dir passed - failure"
-assert_raises "$func '$installation_dir' '$plugin'" "1"
+$func "$installation_dir" "$plugin"
+code="$?"
+assert "echo '$code'" "1"
 
 test_name "no plugin/installation - plugin name validation not called"
 assert "stub_called_times 'habitat_is_valid_plugin_name'" "0"
@@ -37,7 +40,9 @@ setup
 plugin=""
 installation_dir="$tmp"
 test_name "installation dir passed no plugin passed - failure"
-assert_raises "$func '$installation_dir' '$plugin'" "1"
+$func "$installation_dir" "$plugin"
+code="$?"
+assert "echo '$code'" "1"
 
 test_name "no plugin - plugin name validation not called"
 assert "stub_called_times 'habitat_is_valid_plugin_name'" "0"
@@ -51,7 +56,9 @@ setup
 plugin="nope/nope"
 installation_dir="$tmp"
 test_name "installation dir passed and valid plugin passed - success"
-assert_raises "$func '$installation_dir' '$plugin'" "0"
+$func "$installation_dir" "$plugin"
+code="$?"
+assert "echo '$code'" "0"
 
 test_name "Plugin file should now exist - Success"
 assert_raises "test -f '$tmp/$plugin'" "0"
@@ -65,6 +72,25 @@ clean
 #
 #
 setup
+plugin="nope/nope"
+installation_dir="$tmp"
+test_name "installation/plugin but already exists - failure"
+mkdir "$tmp/$(dirname "$plugin")"
+touch "$installation_dir/$plugin"
+$func "$installation_dir" "$plugin"
+code="$?"
+assert "echo '$code'" "1"
+
+test_name "plugin/install - plugin name validation called"
+assert "stub_called_times 'habitat_is_valid_plugin_name'" "1"
+
+clean
+
+
+#
+#
+#
+setup
 # need to make it return false for these tests
 restore 'habitat_is_valid_plugin_name'
 stub_and_eval 'habitat_is_valid_plugin_name' "return 1"
@@ -72,7 +98,9 @@ stub_and_eval 'habitat_is_valid_plugin_name' "return 1"
 plugin="nope/"
 installation_dir="$tmp"
 test_name "installation dir passed and invalid plugin passed - failure"
-assert_raises "$func '$installation_dir' '$plugin'" "1"
+$func "$installation_dir" "$plugin"
+code="$?"
+assert "echo '$code'" "1"
 
 test_name "plugin/install but invalid - plugin name validation called"
 assert "stub_called_times 'habitat_is_valid_plugin_name'" "1"
