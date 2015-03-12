24ff47e9431bf074a0e381195d6bf71a3e6aee95
Brandon Casey <brandonocasey@gmail.com>
2015-03-12 00:40:28 -0400

    started working on write config unit tests

--- t/habitat_write_config.t.sh
+++ t/habitat_write_config.t.sh
@@ -3,6 +3,7 @@ source "$(dirname "$0")/test-helper.sh" "$0" "$@"
 func="habitat_write_config"
 config="$tmp/things.cfg"
 
+# TODO: multiple same config lines
 
 function setup() {
   if [ -d "$tmp" ]; then
@@ -34,7 +35,41 @@ assert_raises "$func" "1"
 
 test_name "Key not passed - Failure"
 assert_raises "$func '$config'" "1"
+
+test_name "Val not passed - Success"
+assert_raises "$func '$config' 'key'" "0"
+
+test_name "Config has key with no value"
+assert "cat '$config'" "key="
+
 clean
+
+#
+#
+#
+setup
+test_name "Config/Key/Val passed success - Success"
+assert_raises "$func '$config' 'key' 'val'" "0"
+
+test_name "Config contains correct value"
+assert "cat '$config'" "key=val"
+
+
+test_name "Change config value"
+assert_raises "$func '$config' 'key' 'val2'" "0"
+
+test_name "Config Value was changed"
+assert "cat '$config'" "key=val2"
+
+test_name "Add config value"
+assert_raises "$func '$config' 'key2' 'val2'" "0"
+
+test_name "Config Value was added"
+assert "cat '$config'" "key=val2\nkey2=val2"
+
+
+clean
+
 
 
 
