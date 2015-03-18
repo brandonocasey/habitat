b46a706d64d0fcf98d532043a7055eac5f3a50cc
Brandon Casey <brandonocasey@gmail.com>
2015-03-18 17:12:17 -0400

    Added colors and better printing

--- test-runner.sh
+++ test-runner.sh
@@ -3,19 +3,26 @@
 TIMEFORMAT=%R
 function run_tests() {
    local total=0
-   local failures=0
+   local successes=0
    local unit_test_file
    for unit_test_file in $(find "$(dirname "$0")"/t -name "*.t.sh"); do
-        echo "*** Running $(basename "$unit_test_file")"
-        $unit_test_file "$@"
-        if [ "$?" != 0 ]; then
-           ((failures=failures+1))
-        fi
-        ((total=total+1))
+      local test_output=""
+      local test_result="*** $(basename "$unit_test_file")$(tput sgr0)"
+      test_output="$($unit_test_file "$@" 2>&1)"
+      if [ "$?" = 0 ]; then
+         test_result="$(tput setaf 2)${test_result}"
+        ((successes=successes+1))
+      else
+         test_result="$(tput setaf 1)${test_result}"
+         test_result="${test_result}
+${test_output}"
+      fi
+      echo "$test_result"
+      ((total=total+1))
    done
    echo ""
-   #use printf to get around the fact that we cannot return
-   printf "$failures/$total Tests Failed"
+   #use printf to get around the fact that weo cannot return
+   printf "$successes/$total Tests Succeeded"
 }
-time_taken=$( time ( run_tests 2>&1 ) 3>&1 1>&2 2>&3 )
+time_taken=$( time ( run_tests "$@" 2>&1 ) 3>&1 1>&2 2>&3 )
 echo " In ${time_taken}s"
