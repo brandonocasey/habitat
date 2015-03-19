ecdb84c682dc8df53a90a068b48a4de7ac482518
Brandon Casey <brandonocasey@gmail.com>
2015-03-18 21:32:55 -0400

    Added habitat_clone_plugin_repo to simplify testing

--- t/habitat_add_plugin.t.sh
+++ t/habitat_add_plugin.t.sh
@@ -3,39 +3,14 @@ source "$(dirname "$0")/test-helper.sh" "$0" "$@"
 func="habitat_add_plugin"
 
 function setup() {
-  if [ -d "$tmp" ]; then
-    rm -rf "$tmp"/*
-  fi
-  while [ $# -gt 0 ]; do
-    local author="${1:-}"; shift
-    local plugin="${1:-}"; shift
-    if [ -z "${author:-}" ]; then
-      continue
-    fi
-    if [ ! -d "$tmp/$author" ]; then
-      mkdir -p "$tmp/$author"
-    fi
-
-    if [ -z "${plugin:-}" ]; then
-      continue
-    fi
-    if [ ! -f "$tmp/$author/$plugin" ];then
-      touch "$tmp/$author/$plugin"
-    fi
-  done
-
   stub 'habitat_write_config'
   stub 'habitat_read_config'
-  stub 'habitat_is_valid_plugin_name'
+  stub 'habitat_clone_plugin_repo'
 }
 function clean() {
   restore 'habitat_write_config'
   restore 'habitat_read_config'
-  restore 'habitat_is_valid_plugin_name'
-
-  if [ -d "$tmp" ]; then
-    rm -rf "$tmp"/*
-  fi
+  restore 'habitat_clone_plugin_repo'
 }
 
 
@@ -52,9 +27,9 @@ assert_raises "$func '1'" "1"
 test_name 'Two Args - Failure'
 assert_raises "$func '1' '$tmp'" "1"
 
-restore 'habitat_is_valid_plugin_name'
-stub_and_eval 'habitat_is_valid_plugin_name' 'return 1'
-test_name 'Three Args Invalid plugin name - Failure'
+restore 'habitat_clone_plugin_repo'
+stub_and_eval 'habitat_clone_plugin_repo' 'return 1'
+test_name 'Three Args Clone Plugin Repo Fails - Failure'
 assert_raises "$func '1' '$tmp' 'author/plugin'" "1"
 
 clean
@@ -62,107 +37,45 @@ clean
 #
 #
 #
-setup "author" "name" "thing" "thing"
+setup
 $func '1' "$tmp" 'author/name'
 code="$?"
-test_name 'Remove an exisiting plugin'
+test_name 'Add a plugin dont save to config'
 assert "echo '$code'" "0"
 
-test_name 'verify that valid plugin name was called'
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "1"
+test_name 'verify that habitat_clone_plugin_repo is called'
+assert "stub_called_times 'habitat_clone_plugin_repo'" "1"
 
-test_name 'verify the file was deleted'
-assert_raises "test -f '$tmp/author/name'" "1"
+# TODO: Find out how to test this one
+# test_name 'verify habitat_read_config was not called'
+# assert "stub_called_times 'habitat_read_config'" "0"
 
-test_name 'verify that another author was not deleted'
-assert_raises "test -f '$tmp/thing/thing'" "0"
-
-test_name 'verify the author was deleted as it has no more files'
-assert_raises "test -d '$tmp/author'" "1"
-clean
-
-#
-# verify that the author dir is not deleted if they have more than 1 plugin
-#
-setup "author" "name" "author" "thing"
-$func '1' "$tmp" 'author/name'
-test_name 'verify that the author was not deleted'
-assert_raises "test -d '$tmp/author'" "0"
-
-test_name 'verify the authors plugin was not deleted'
-assert_raises "test -f '$tmp/author/thing'" "0"
-
-test_name 'verify that valid plugin name was called'
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "1"
+test_name 'verify habitat_write_config was not called'
+assert "stub_called_times 'habitat_write_config'" "0"
 
 clean
 
-
 #
 #
 #
-setup "author" "test"
-$func '1' "$tmp" 'author/name'
+setup
+$func 'config.cfg' "$tmp" 'author/name'
 code="$?"
-test_name 'Remove a non-existing plugin'
-assert "echo '$code'" "1"
-
-test_name 'verify the nothing was deleted'
-assert_raises "test -f '$tmp/author/test'" "0"
-
-test_name 'verify that valid plugin name was called'
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "1"
-
-
-clean
-
-
-#
-#
-#
-setup "author" "name"
-$func 'config_file.cfg' "$tmp" 'author/name'
-code="$?"
-test_name 'Remove an existing plugin and save to config'
+test_name 'Add a plugin save to config'
 assert "echo '$code'" "0"
 
-test_name 'verify that write config was called'
+test_name 'verify that habitat_clone_plugin_repo is called'
+assert "stub_called_times 'habitat_clone_plugin_repo'" "1"
+
+# TODO: Find out how to test this one
+# test_name 'verify habitat_read_config was called'
+# assert "stub_called_times 'habitat_read_config'" "1"
+
+test_name 'verify habitat_write_config was called'
 assert "stub_called_times 'habitat_write_config'" "1"
 
-test_name 'verify that valid plugin name was called'
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "1"
-
-
-# TODO: how to capture a function call through an aubshell output capture $()
-# test_name 'verify that read config was called'
-# assert "stub_called_with habitat_read_config 'config_file.cfg'" "1"
 
 clean
-
-#
-#
-#
-setup "author" "test"
-$func 'config_file.cfg' "$tmp" 'author/name'
-code="$?"
-test_name 'Remove an non-existing plugin and save to config'
-assert "echo '$code'" "1"
-
-test_name 'verify that write config was still called'
-assert "stub_called_times habitat_write_config" "1"
-
-test_name 'verify that valid plugin name was called'
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "1"
-
-
-# TODO: how to capture a function call through an aubshell output capture $()
-# test_name 'verify that write read config was still called'
-# assert "stub_called_times habitat_read_config" "1"
-
-clean
-
-
-
 
 
 assert_end "$func"
