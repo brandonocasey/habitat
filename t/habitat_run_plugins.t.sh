d7d207d97807a4b2bd3c77fac0ea37bc04fd18df
Brandon Casey <brandonocasey@gmail.com>
2015-03-23 10:51:24 -0400

    fixed habitat run plugins and unit tests for it

--- t/habitat_run_plugins.t.sh
+++ t/habitat_run_plugins.t.sh
@@ -1,148 +1,102 @@
 #!/usr/bin/env bash
 source "$(dirname "$0")/test-helper.sh" "$0" "$@"
-func="habitat_run_plugins"
 
 
 function setup() {
   stub 'habitat_call_function'
-  stub 'habitat_is_valid_plugin_name'
 }
 function clean() {
   restore 'habitat_call_function'
-  restore 'habitat_is_valid_plugin_name'
 }
 
-
-#
-#
-#
 setup
-plugins=""
-test_name "no plugins - no output"
-assert "$func '$plugins'" ""
+  scenario_name "No Args"
+  dir=""
+  plugin_list=""
+  expected_code="1"
+  call_function_called_times="0"
+  habitat_run_plugins "$dir" "$plugin_list"
+  code="$?"
 
-test_name "no plugins - failure"
-assert_raises "$func '$plugins'" "1"
+  test_name "Function Result Code $expected_code"
+  assert "echo '$code'" "$expected_code"
 
-test_name "no plugins - no functions called"
-$func "$plugins" 2>&1 > /dev/null
-assert "stub_called_times 'habitat_call_function'" "0"
+  test_name "habitat_call_function called $call_function_called_times times"
+  assert "stub_called_times 'habitat_call_function'" "$call_function_called_times"
+clean
 
-test_name "no plugins - no plugin name validation called"
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "0"
+setup
+  scenario_name "One Arg"
+  dir="$tmp"
+  plugin_list=""
+  expected_code="1"
+  call_function_called_times="0"
+  habitat_run_plugins "$dir" "$plugin_list"
+  code="$?"
+
+  test_name "Function Result Code $expected_code"
+  assert "echo '$code'" "$expected_code"
+
+  test_name "habitat_call_function called $call_function_called_times times"
+  assert "stub_called_times 'habitat_call_function'" "$call_function_called_times"
 clean
 
 
-#
-#
-#
 setup
-plugins="thing/thing"
-test_name "1 plugins no functions - no output"
-assert "$func '$plugins'" ""
+  scenario_name "Two Valid Args One Plugin"
+  dir="$tmp"
+  plugin_list="one/one"
+  expected_code="0"
+  call_function_called_times="1"
+  habitat_run_plugins "$dir" "$plugin_list"
+  code="$?"
 
-test_name "1 plugins no functions - success"
-assert_raises "$func '$plugins'" "0"
+  test_name "Function Result Code $expected_code"
+  assert "echo '$code'" "$expected_code"
 
-test_name "1 plugins no functions - no functions called"
-$func "$plugins" 2>&1 > /dev/null
-assert "stub_called_times 'habitat_call_function'" "1"
-
-test_name "1 plugin - plugin name validation called"
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "1"
+  test_name "habitat_call_function called $call_function_called_times times"
+  assert "stub_called_times 'habitat_call_function'" "$call_function_called_times"
 clean
 
-
-#
-#
-#
 setup
-plugins="thing/thing"
-test_name "1 plugins run function - no output"
-assert "$func '$plugins'" ""
+  scenario_name "Two Valid Args Two Plugin"
+  dir="$tmp"
+  plugin_list="one/one
+two/two"
+  expected_code="0"
+  call_function_called_times="2"
+  habitat_run_plugins "$dir" "$plugin_list"
+  code="$?"
 
-test_name "1 plugins run function - success"
-assert_raises "$func '$plugins'" "0"
+  test_name "Function Result Code $expected_code"
+  assert "echo '$code'" "$expected_code"
 
-test_name "1 plugins run function - 1 function called"
-$func "$plugins" 2>&1 > /dev/null
-assert "stub_called_times 'habitat_call_function'" "1"
-
-test_name "1 plugin - plugin name validation called"
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "1"
-
+  test_name "habitat_call_function called $call_function_called_times times"
+  assert "stub_called_times 'habitat_call_function'" "$call_function_called_times"
 clean
 
-
-
-#
-#
-#
 setup
-plugins="thing/thing thing/wings"
+  scenario_name "Two Valid Args One Plugin Call Failure"
+  restore "habitat_call_function"
+  stub_and_eval "habitat_call_function" "return 1"
+  restore "habitat_error"
+  stub "habitat_error"
+  dir="$tmp"
+  plugin_list="one/one"
+  expected_code="0"
+  call_function_called_times="1"
+  habitat_run_plugins "$dir" "$plugin_list"
+  code="$?"
 
-test_name "2 plugins 1 with run function - no output"
-assert "$func '$plugins'" ""
+  test_name "Function Result Code $expected_code"
+  assert "echo '$code'" "$expected_code"
 
-test_name "2 plugins 1 with run function - success"
-assert_raises "$func '$plugins'" "0"
+  test_name "habitat_call_function called $call_function_called_times times"
+  assert "stub_called_times 'habitat_call_function'" "$call_function_called_times"
 
-test_name "2 plugins 1 with run function - 2 function called"
-$func "$plugins" 2>&1 > /dev/null
-assert "stub_called_times 'habitat_call_function'" "2"
-
-test_name "2 plugins - plugin name validation called twice"
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "2"
-
+  test_name "habitat_error called 1 time"
+  assert "stub_called_times 'habitat_error'" "1"
 clean
-
-
-#
-#
-#
-setup
-plugins="thing/thing thing/wings"
-test_name "2 plugins both with a run function - no output"
-assert "$func '$plugins'" ""
-
-test_name "2 plugins both with a run function - success"
-assert_raises "$func '$plugins'" "0"
-
-test_name "2 plugins both with a run function - 2 function called"
-$func "$plugins" 2>&1 > /dev/null
-assert "stub_called_times 'habitat_call_function'" "2"
-
-test_name "2 plugins - plugin name validation called twice"
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "2"
-
-clean
-
-
-
-#
-#
-#
-setup
-plugins="thing/thing"
-# need to make it return false for these tests
-restore 'habitat_is_valid_plugin_name'
-stub_and_eval 'habitat_is_valid_plugin_name' "return 1"
-
-test_name "1 plugins run function, invalid plugin name - no output"
-assert "$func '$plugins'" ""
-
-test_name "1 plugins run function, invalid plugin name - success"
-assert_raises "$func '$plugins'" "0"
-
-test_name "1 plugins run function, all invalid - 0 functions called"
-$func "$plugins" 2>&1 > /dev/null
-assert "stub_called_times 'habitat_call_function'" "0"
-
-test_name "1 plugin - plugin name validation called"
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "1"
-
-clean
-
 
 
 
