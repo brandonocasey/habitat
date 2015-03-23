4b1c052d28448e0816630975ca6750622a395c55
Brandon Casey <brandonocasey@gmail.com>
2015-03-23 10:37:51 -0400

    Fixed habitat_source and also fixed the unit tests

--- t/habitat_source_plugins.t.sh
+++ t/habitat_source_plugins.t.sh
@@ -1,227 +1,78 @@
 #!/usr/bin/env bash
 source "$(dirname "$0")/test-helper.sh" "$0" "$@"
-func="habitat_source_plugins"
 
 
 function setup() {
   stub 'habitat_source_file'
-  stub 'habitat_is_valid_plugin_name'
-  if [ -d "$tmp" ]; then
-    rm -rf "$tmp"/*
-  fi
-  while [ $# -gt 0 ]; do
-    local author="${1:-}"; shift
-    local plugin="${1:-}"; shift
-    local habit_file="${1:-}"; shift
-    if [ -z "${author:-}" ]; then
-      continue
-    fi
-    if [ ! -d "$tmp/$author" ]; then
-      mkdir -p "$tmp/$author"
-    fi
-
-    if [ -z "${plugin:-}" ]; then
-      continue
-    fi
-    if [ ! -d "$tmp/$author/$plugin" ];then
-      mkdir -p "$tmp/$author/$plugin"
-    fi
-    if [ "$habit_file" = "0" ]; then
-      touch "$tmp/$author/$plugin/habit"
-    fi
-  done
 }
 function clean() {
   restore 'habitat_source_file'
-  restore 'habitat_is_valid_plugin_name'
-  if [ -d "$tmp" ]; then
-    rm -rf "$tmp"/*
-  fi
 }
 
-
-#
-#
-#
 setup
-test_name "No Authors/Plugins - No STDOUT"
-assert "$func '$tmp'" ""
+  scenario_name "No Args"
+  dir=""
+  plugin_list=""
+  expected_code="1"
+  source_called_times="0"
+  habitat_source_plugins "$dir" "$plugin_list"
+  code="$?"
 
-test_name "No Authors/Plugins - Failure"
-assert_raises "$func '$tmp'" "1"
+  test_name "Function Result Code $expected_code"
+  assert "echo '$code'" "$expected_code"
 
-test_name "No Authors/Plugins - source not called"
-$func "$tmp" 2>&1 > /dev/null
-assert "stub_called_times 'habitat_source_file'" "0"
+  test_name "habitat_source_file called $source_called_times times"
+  assert "stub_called_times 'habitat_source_file'" "$source_called_times"
+clean
 
-test_name "No Authors/Plugins - plugin validation not called"
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "0"
+setup
+  scenario_name "One Arg"
+  dir="$tmp"
+  plugin_list=""
+  expected_code="1"
+  source_called_times="0"
+  habitat_source_plugins "$dir" "$plugin_list"
+  code="$?"
 
+  test_name "Function Result Code $expected_code"
+  assert "echo '$code'" "$expected_code"
+
+  test_name "habitat_source_file called $source_called_times times"
+  assert "stub_called_times 'habitat_source_file'" "$source_called_times"
+clean
+
+setup
+  scenario_name "Two Args One Plugin"
+  dir="$tmp"
+  plugin_list="author/repo"
+  expected_code="0"
+  source_called_times="1"
+  habitat_source_plugins "$dir" "$plugin_list"
+  code="$?"
+
+  test_name "Function Result Code $expected_code"
+  assert "echo '$code'" "$expected_code"
+
+  test_name "habitat_source_file called $source_called_times times"
+  assert "stub_called_times 'habitat_source_file'" "$source_called_times"
+clean
+
+setup
+  scenario_name "Two Args Two Plugin"
+  dir="$tmp"
+  plugin_list="author/repo
+no/no"
+  expected_code="0"
+  source_called_times="2"
+  habitat_source_plugins "$dir" "$plugin_list"
+  code="$?"
+
+  test_name "Function Result Code $expected_code"
+  assert "echo '$code'" "$expected_code"
+
+  test_name "habitat_source_file called $source_called_times times"
+  assert "stub_called_times 'habitat_source_file'" "$source_called_times"
 clean
 
 
-
-#
-#
-#
-setup "author"
-test_name "1 Authors 0 Plugins - No STDOUT"
-assert "$func '$tmp'" ""
-
-test_name "1 Authors 0 Plugins - Success"
-assert_raises "$func '$tmp'" "0"
-
-test_name "1 Authors 0 Plugins - source not called"
-$func "$tmp" 2>&1 > /dev/null
-assert "stub_called_times 'habitat_source_file'" "0"
-
-test_name "1 Author 0 Plugins - plugin validation not called"
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "0"
-
-clean
-
-
-#
-#
-#
-setup "author" "plugin" "0"
-test_name "1 Authors 1 Plugins - STDOUT"
-assert "$func '$tmp'" "author_plugin"
-
-test_name "1 Authors 1 Plugins - Success"
-assert_raises "$func '$tmp'" "0"
-
-
-test_name "1 Authors 1 Plugins - source called"
-$func "$tmp" 2>&1 > /dev/null
-assert "stub_called_times 'habitat_source_file'" "1"
-
-test_name "1 Author 1 Plugin - plugin validation called"
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "1"
-
-clean
-
-#
-#
-#
-setup "author" "plugin" "0" "author" "plugin2" "0"
-test_name "1 Authors 2 Plugins - STDOUT"
-assert "$func '$tmp'" "author_plugin\nauthor_plugin2"
-
-test_name "1 Authors 2 Plugins - Success"
-assert_raises "$func '$tmp'" "0"
-
-
-test_name "1 Authors 2 Plugins - source called"
-$func "$tmp" 2>&1 > /dev/null
-assert "stub_called_times 'habitat_source_file'" "2"
-
-test_name "1 Author 2 Plugin - plugin validation called twice"
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "2"
-
-clean
-
-
-
-#
-#
-#
-setup "author" "plugin" "0" "author2"
-test_name "2 Authors 1 Plugin - STDOUT"
-assert "$func '$tmp'" "author_plugin"
-
-test_name "2 Authors 1 Plugin - Success"
-assert_raises "$func '$tmp'" "0"
-
-
-test_name "2 Authors 1 Plugin - source called"
-$func "$tmp" 2>&1 > /dev/null
-assert "stub_called_times 'habitat_source_file'" "1"
-
-test_name "2 Author 1 Plugin - plugin validation called once"
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "1"
-
-clean
-
-
-#
-#
-#
-setup "author" "plugin" "0" "author2" "plugin2" "0"
-test_name "2 Authors 1 Plugin Each - STDOUT"
-assert "$func '$tmp'" "author_plugin\nauthor2_plugin2"
-
-test_name "2 Authors 1 Plugin Each - Success"
-assert_raises "$func '$tmp'" "0"
-
-
-test_name "2 Authors 1 Plugin Each - source called"
-$func "$tmp" 2>&1 > /dev/null
-assert "stub_called_times 'habitat_source_file'" "2"
-
-test_name "2 Author 1 Plugin Each - plugin validation called twice"
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "2"
-
-clean
-
-
-#
-#
-#
-setup "author" "plugin" "0" "author" "plugin2" "0" "author2" "plugin" "0" "author2" "plugin2" "0"
-test_name "2 Authors 2 Plugin Each - STDOUT"
-assert "$func '$tmp'" "author_plugin\nauthor_plugin2\nauthor2_plugin\nauthor2_plugin2"
-
-test_name "2 Authors 2 Plugin Each - Success"
-assert_raises "$func '$tmp'" "0"
-
-test_name "2 Authors 2 Plugin Each - source called"
-$func "$tmp" 2>&1 > /dev/null
-assert "stub_called_times 'habitat_source_file'" "4"
-
-test_name "2 Author 2 Plugin Each - plugin validation called 4 times"
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "4"
-
-clean
-
-
-# Verify validation returing false
-setup "author" "plugin" "0" "author" "plugin2" "0"
-# need to make it return false for these tests
-restore 'habitat_is_valid_plugin_name'
-stub_and_eval 'habitat_is_valid_plugin_name' "return 1"
-
-test_name "1 Authors 2 Plugins, all invalid - STDOUT"
-assert "$func '$tmp'" ""
-
-test_name "1 Authors 2 Plugins, all invalid - Success"
-assert_raises "$func '$tmp'" "0"
-
-
-test_name "1 Authors 2 Plugins, all invalid - source not called"
-$func "$tmp" 2>&1 > /dev/null
-assert "stub_called_times 'habitat_source_file'" "0"
-
-test_name "1 Author 2 Plugin, all invalid - plugin validation called twice"
-assert "stub_called_times 'habitat_is_valid_plugin_name'" "2"
-
-clean
-
-# Verify no habit file
-setup "author" "plugin" "1"
-test_name "1 Authors 1 Plugins no habit file - STDOUT"
-assert "$func '$tmp'" ""
-
-test_name "1 Authors 1 Plugins no habit file - Success"
-assert_raises "$func '$tmp'" "0"
-
-sest_name "1 Authors 1 Plugins no habit file - source not called"
-$func "$tmp" 2>&1 > /dev/null
-assert "stub_called_times 'habitat_source_file'" "0"
-
-clean
-
-
-
-
-assert_end "$func"
+assert_end "habitat_source_plugins"
