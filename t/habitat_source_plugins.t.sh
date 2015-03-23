dfd5269ba8792f9c257d69e4b657b3953cdaab0a
Brandon Casey <brandonocasey@gmail.com>
2015-03-23 10:57:36 -0400

    Added a test to check for habitat_error, also testing for blank newlines

--- t/habitat_source_plugins.t.sh
+++ t/habitat_source_plugins.t.sh
@@ -58,6 +58,25 @@ setup
 clean
 
 setup
+  scenario_name "Two Args One Plugin with blank newlines"
+  dir="$tmp"
+  plugin_list="
+author/repo
+"
+  expected_code="0"
+  source_called_times="1"
+  habitat_source_plugins "$dir" "$plugin_list"
+  code="$?"
+
+  test_name "Function Result Code $expected_code"
+  assert "echo '$code'" "$expected_code"
+
+  test_name "habitat_source_file called $source_called_times times"
+  assert "stub_called_times 'habitat_source_file'" "$source_called_times"
+clean
+
+
+setup
   scenario_name "Two Args Two Plugin"
   dir="$tmp"
   plugin_list="author/repo
@@ -72,6 +91,32 @@ no/no"
 
   test_name "habitat_source_file called $source_called_times times"
   assert "stub_called_times 'habitat_source_file'" "$source_called_times"
+clean
+
+
+setup
+  scenario_name "Two Args One Plugin Source fails"
+  restore 'habitat_error'
+  stub 'habitat_error'
+
+  restore "habitat_source_file"
+  stub_and_eval 'habitat_source_file' "return 1"
+  dir="$tmp"
+  plugin_list="author/repo"
+  expected_code="0"
+  source_called_times="1"
+  habitat_source_plugins "$dir" "$plugin_list"
+  code="$?"
+
+  test_name "Function Result Code $expected_code"
+  assert "echo '$code'" "$expected_code"
+
+  test_name "habitat_source_file called $source_called_times times"
+  assert "stub_called_times 'habitat_source_file'" "$source_called_times"
+
+  test_name "habitat_error called 1 times"
+  assert "stub_called_times 'habitat_error'" "1"
+
 clean
 
 
